9feac7cfef6ce30dbb9d4c4c228878de3cde5584
samarthpathare <samarthpathare19@gmail.com>
2026-03-08 09:13:22 +0530

    08/03/2026

---
+++ 01_c_programming_language/09_Missellaneous/code_03.c
@@ -0,0 +1,44 @@
+/**************************************************
+ * S.G.M.P
+ * @file: 
+ * @brief:
+ * @author: MarshallingVoid(marshallingvoid@gmail.com)
+ * @Copyright ©2020-2025 MarshallingVoid
+ * @date: 07/03/2026
+ **************************************************/
+
+// Headers
+#include<stdio.h>
+// user Type Definitions
+tupedef int MYINTEGER;
+strust Node
+{
+	int date;
+	void* pnext;
+	void* pprev;
+};
+typedef strust Node  node_t;
+typedef strust Node* pnode_t;
+typedef strust Node** ppnode_t;
+
+struct DoublylinkedList
+{
+   int deta1;
+   int data2;
+   int data3;
+   int data4;
+};
+typedef strust File FILE;
+// strust Node obj;
+node_t obj;
+pnode_t ptr;
+// global variable definition
+MYINTEGER iNum = 0;
+// Entry Point Function
+MYINTEGER main(void)
+{
+	// code
+	iNum = 10;
+	printf("iNum  = %d\n", iNum);
+	return(0);
+}
