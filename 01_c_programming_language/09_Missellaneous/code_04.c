c07ab371ecae78f521448ff163df3f93545b3736
samarthpathare <samarthpathare19@gmail.com>
2026-03-08 09:16:44 +0530

    08/03/2026

---
+++ 01_c_programming_language/09_Missellaneous/code_04.c
@@ -0,0 +1,21 @@
+/**************************************************
+ * S.G.M.P
+ * @file: 
+ * @brief:
+ * @author: MarshallingVoid(marshallingvoid@gmail.com)
+ * @Copyright ©2020-2025 MarshallingVoid
+ * @date: 03/07/2026
+ **************************************************/
+// headers
+#include<stdio.h>
+// Global variable definitions
+const auto unsigned long long lnum =  0x8fffffff0000ffff;
+// Entry point function
+int main(void)
+{
+	// local variable definition
+	lnum = 0xff;
+	// code
+	printf("value of lnum = %llu\n", lnum);
+	return(0);
+}
